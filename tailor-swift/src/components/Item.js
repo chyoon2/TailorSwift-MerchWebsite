3f9fe3c1f672ca11625a514565a5b447d33284cd
Chris Yoon <chy.yoon@gmail.com>
2020-09-09 11:23:21 -0700

    our love component
    Co-authored-by: Mariel Hamson <mariel.hamson@gmail.com>
    Co-authored-by: Kevin Davis <thekidnamedkd@gmail.com>

---
+++ tailor-swift/src/components/Item.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Item(props) {
+  return (
+    <React.Fragment>
+      <h3>{props.name}</h3>
+      <h3>{props.size}</h3>
+      <h3>{props.quantity}</h3>
+      <h3>{props.description}</h3>
+      <h3>{props.category}</h3>
+    </React.Fragment>
+  )
+}
+
+Item.propTypes = {
+  category: PropTypes.string.isRequired,
+  size: PropTypes.string.isRequired,
+  quantity: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired
+}
+
+export default Item;
